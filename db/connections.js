2293b7d9b835315bfe36bffbaec4b3c8e28f8cfb
nguyent0275 <100709940+nguyent0275@users.noreply.github.com>
2024-01-20 10:25:44 -0500

    added demo video

--- db/connections.js
+++ db/connections.js
@@ -1,11 +1,6 @@
 require("dotenv").config();
 const mysql = require('mysql2/promise')
 
-// this file will read .env file and look for these variables
-// DB_HOST=
-// DB_USER=
-// DB_PASSWORD=
-// DB_DATABASE=
 const startConnection = async () => {
     // returns a db variable that we can use
   return await mysql.createConnection({
